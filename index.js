b66196f8a5e5b132955f09ade895283944a13ff0
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-19 17:05:28 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://DB_USER:DB_PASS@cluster0.ady3n.mongodb.net/?retryWrites=true&w=majority";
+
 app.get("/", (req, res) => {
   res.send("Hello from Doctor Portal");
 });
 
 app.listen(port, () => {
-  console.log(`DOctors App listening on port ${port}`);
+  console.log(`Doctors App listening on port ${port}`);
 });
